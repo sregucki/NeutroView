1e7feafad8b856792caafec0e678aa806d9394e4
sregucki <sebastian.regucki@outlook.com>
2023-11-23 13:58:35 +0100

    news api transformed request body to query params

--- server/api/articles.py
+++ server/api/articles.py
@@ -1,3 +1,5 @@
+from datetime import date
+from typing import Optional
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
@@ -8,8 +10,28 @@ router = APIRouter()
 
 
 @router.get("/")
-async def get_articles(query: ArticleQuery):
-    articles = ArticleService(query).get_articles()
+async def get_articles(
+    keyword: Optional[str] = None,
+    category: Optional[str] = "politics",
+    timespan: Optional[str] = "1d",
+    num_records: Optional[int] = 10,
+    domain: Optional[str] = None,
+    start_date: Optional[date] = None,
+    end_date: Optional[date] = None,
+    country: Optional[str] = None,
+):
+    articles = ArticleService(
+        ArticleQuery(
+            keyword=keyword,
+            category=category,
+            timespan=timespan,
+            num_records=num_records,
+            domain=domain,
+            start_date=start_date,
+            end_date=end_date,
+            country=country,
+        )
+    ).get_articles()
     if not articles:
         return JSONResponse(
             content={"message": "No articles found for given filters."}, status_code=404
