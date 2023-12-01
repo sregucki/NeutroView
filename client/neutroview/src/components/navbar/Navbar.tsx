e4174acc536f875f4c2e94c680ba8d85a0275360
sregucki <sebastian.regucki@outlook.com>
2023-12-01 20:04:26 +0100

    Basic navbar setup

--- client/neutroview/src/components/navbar/Navbar.tsx
+++ client/neutroview/src/components/navbar/Navbar.tsx
@@ -1,3 +1,52 @@
-function Navbar() {}
+import { useEffect, useState } from "react";
+import "./navbar.scss";
+
+function Navbar() {
+  const [currentDatetime, setCurrentDatetime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDatetime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div className="navbar-main">
+      <div className="navbar-top">
+        <div className="navbar-main-content">
+          <div className="navbar-top-item-datetime">
+            <span>
+              {currentDatetime.toLocaleDateString("en-US", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+              })}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="navbar-bottom">
+        <div className="navbar-main-content">
+          <div className="navbar-bottom-item">
+            <a href="/">Home</a>
+          </div>
+          <div className="navbar-bottom-item">
+            <a>Local</a>
+          </div>
+          <div className="navbar-bottom-item">
+            <a>European Poltics</a>
+          </div>
+          <div className="navbar-bottom-item">
+            <a>US</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Navbar;
