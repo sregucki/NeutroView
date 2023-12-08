490841bf865cd25f8f24d2f18bc9193e228a5e16
sregucki <sebastian.regucki@outlook.com>
2023-12-08 11:28:14 +0100

    daily local news section without

--- client/neutroview/src/components/home/Home.tsx
+++ client/neutroview/src/components/home/Home.tsx
@@ -1,9 +1,45 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Navbar from "../navbar/Navbar";
+import "./home.scss";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
-    return (
-        <h1>Home</h1>
-    )
+  return (
+    <div>
+      <Navbar />
+      <div className="main-container">
+        <div className="container" id="top-news-stories">
+          1
+        </div>
+        <div className="container" id="headlines">
+          2
+        </div>
+        <div className="container" id="local-news">
+          <h2>Daily Local News</h2>
+          <div className="container-title">
+            <span className="title-smaller">
+              Discover stories happening right in your city.
+            </span>
+          </div>
+          <div id="local-news-search-container">
+            <div id="local-news-search">
+              <input type="text" placeholder="Enter your city's name" />
+            </div>
+            <div id="local-news-search-button">
+              <button>Submit</button>
+            </div>
+          </div>
+          <div className="divider-thick"></div>
+          <div id="set-location-container">
+            <button>
+              <FontAwesomeIcon icon={faLocationDot} /> Set Location
+            </button>
+          </div>
+          <div className="divider-thin"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
