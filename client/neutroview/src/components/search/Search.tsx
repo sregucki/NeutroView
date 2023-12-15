b94e8137cb85b1d89635ddc257677d51b645cc53
sregucki <sebastian.regucki@outlook.com>
2023-12-15 18:03:15 +0100

    Search by keyword page

--- client/neutroview/src/components/search/Search.tsx
+++ client/neutroview/src/components/search/Search.tsx
@@ -1,7 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+import { fetchArticlesFromApi, getTopArticleProviders } from "../../services/ArticleService";
+import { IArticle } from "../../types/ApiTypes";
 import NavbarLite from "../navbar/NavbarLite";
+import "./search.scss";
 
 function Search() {
-  return <NavbarLite />;
+  const wasCalled = useRef(false);
+  const keyword = window.location.href.split("keyword")[1].replace("=", "");
+  const [articles, setArticles] = useState<IArticle[]>([]);
+
+  useEffect(() => {
+    if (wasCalled.current) return;
+    wasCalled.current = true;
+    fetchArticlesFromApi(
+      {
+        keyword: keyword,
+        country: "UK,US",
+        category: "",
+        timespan: "1m",
+        num_records: 10,
+        domain: getTopArticleProviders(),
+      },
+      setArticles
+    );
+  }, []);
+
+  return (
+    <div>
+      <NavbarLite />
+      <div className="main-container-search">
+        <div className="news-story-container-main">
+          {articles.map((article) => (
+            <div className="story-container-long" key={article.id}>
+              <div className="story-container-long-desc">
+                <h4>
+                  <a href={article.url}>{article.title}</a>
+                </h4>
+                <span>{`${article.seenDate}, ${article.domain}`}</span>
+              </div>
+              <img src={article.imgUrl}></img>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
