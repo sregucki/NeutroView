05da8d933d2df1bbc72cdff64f3edfe9dd634c02
sregucki <sebastian.regucki@outlook.com>
2023-12-22 12:22:44 +0100

    refactoring in text analysis api

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ from api import articles, text_analysis
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 app.include_router(articles.router, prefix="/articles", tags=["articles"])
-app.include_router(text_analysis.router, prefix="/analysis", tags=["analysis"])
+app.include_router(text_analysis.router, prefix="/text_analysis", tags=["text_analysis"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
